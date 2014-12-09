565a7127f243d7ddf83e02f33c5d43f2c5f40a00
Prateek <luckyprateek2002@gmail.com>
2014-12-08 20:14:50 -0600

    Error handling, requester creation
    - Creating requester first then adding tickets to avoid 409 errors.
    - General error messages and logging for any possible errors that might
    be encountered

--- app.js
+++ app.js
@@ -4,18 +4,21 @@
   csvParser.RELAXED = true;
 
   return {
-    //global vars
+    /*global vars*/
     parsedCSV: null,
     subject: null,
     description: null,
     requesterEmail: null,
     requesterName: null,
     productArea: null,
+    requesterID: null,
+    errorTicketCreate: null,
 
     events: {
       'click .creator': 'getCSVFile',
-      'getCSV.fail': 'errorMessage',
+      'getCSV.fail': 'errorGettingCSV',
       'getCSV.done': 'parseCSV',
+      'searchRequester.done': 'makeRequester'
     }, //end of events
 
     getCSVFile: function() {
@@ -23,39 +26,65 @@
       this.requesterEmail = this.$(".email").val(); //setting requester email from input field
       this.productArea = this.$("#productArea option:selected").val(); //setting product area from dropdown
 
-      /*Checking if requesterName and requester Email was set*/
-      if (this.requesterName.length === 0 || this.requesterEmail.length === 0) {
-        services.notify('Requester Name or Email Cannot Be Blank', 'alert');
+      /*Checking if requesterName and requesterEmail was set*/
+      if (this.requesterName.length === 0) {
+        services.notify('Trainer Name is Required!', 'alert', 8000);
+        return;
+      } else if (this.requesterEmail.length === 0) {
+        services.notify('Trainer Email is Required!', 'alert', 8000);
         return;
       }
 
       this.ajax('getCSV', this.productArea); //getting CSV file
     }, //end of getCSVFile
 
-    errorMessage: function() {
+    errorGettingCSV: function() {
       services.notify("Unable to retrieve file!", 'error');
-    }, //end of errorMessage
+    }, //end of errorGettingCSV
 
     parseCSV: function(data) {
-      this.parsedCSV = csvParser.parse(data); //parseCSV and set parsedCSV var
-      this.makeTickets(); //createTickets
+      this.parsedCSV = csvParser.parse(data); //parse CSV file and set parsedCSV var
+      this.ajax('searchRequester', this.requesterEmail); // search if specified requester exits
     }, //end of parseCSV
 
+    makeRequester: function(data) {
+      if (data.count !== 0) { // if there was a result from the search
+        this.requesterID = data.results[0].id; // set requesterID to by grabbing userID from search result
+        this.makeTickets(); // call on function to create tickets
+      } else { // if the requester doesn't exist already, create the requester as a new user to avoid 409 errors using the ticket import API
+        this.ajax('createRequester', this.requesterName, this.requesterEmail) // creating the requester as a new user
+          .done(function(jqXHR) {
+            this.requesterID = jqXHR.user.id; // grabbing the userID of the requester that was created and setting global var
+            this.makeTickets(); // call on fucntion to create tickets
+          })
+          .fail(function(jqXHR) {
+            /*Upon failure construct an error message with response code and text*/
+            console.log('Error creating user:\n' + jqXHR.status + ' ' + jqXHR.responseText);
+            services.notify('Error creating user!', 'error', 8000); // fail error message
+          });
+      }
+    }, //end of makeRequester
 
     makeTickets: function() {
-
-      var that = this;
+      var that = this; // aliasing this to another var to use inside _.each function
       _.each(this.parsedCSV, function(each) { //iterate over parsedCSV
         that.subject = each[0]; //setting subject
         that.description = each[1]; //setting description
-        that.ajax('importTicket', that.subject, that.description, that.requesterName, that.requesterEmail).done(function(data) {
-            console.log("Created a ticket");
+        that.ajax('importTicket', that.subject, that.description, that.requesterID) // call to create tickets
+          .done(function(data) {
+            console.log('Created a Ticket'); // console logging upon success for debugging
           })
-          .fail(function() {
-            console.log("Request failed");
-          }); //making request to import ticket
-      });
-      services.notify('Done Creating Tickets!', 'notice');
+          .fail(function(jqXHR) {
+            console.log('Ticket Creation failed\n' + jqXHR.status + ' ' + jqXHR.responseText); // console logging error for easy debugging
+            that.errorTicketCreate = true;
+          }); // end of ajax call to create tickets
+      }); // end of _.each to iterate over parsedCSV
+      /*Messages upon successful or unsuccessful ticket creation*/
+      if (this.errorTicketCreate === null) {
+        services.notify('Successfully Created Tickets! :)', 'notice', 8000); // notify success message
+      } else if (this.errorTicketCreate === true) {
+        services.notify('Error in Creating tickets :(', 'error', 8000); // notify error message
+      }
     }, //end of makeTickets
 
     requests: {
@@ -71,17 +100,37 @@
         };
       }, //end of getCSV
 
-      importTicket: function(subject, description, requesterName, requesterEmail) {
+      searchRequester: function(email) {
+        return {
+          url: '/api/v2/search?query=type%3Auser%20email%3A' + encodeURI(email),
+          type: 'GET'
+        };
+      },
+
+      createRequester: function(name, email) {
+        return {
+          url: '/api/v2/users.json',
+          type: 'POST',
+          contentType: 'application/json',
+          data: JSON.stringify({
+            'user': {
+              'name': name,
+              'email': email
+            }
+          })
+        };
+
+      }, //end of createRequester
+
+      importTicket: function(subject, description, requester_id) {
           return {
             url: '/api/v2/imports/tickets.json',
             type: 'POST',
             contentType: 'application/json', //data:json doesn't work O.o
             data: JSON.stringify({
               "ticket": {
-                "requester": {
-                  "name": requesterName,
-                  "email": requesterEmail
-                },
+                "requester_id": requester_id,
+                "tags": [ "zendesk_training", "zendesk_training_t2" ],
                 "subject": subject,
                 "comment": {
                   "body": description
